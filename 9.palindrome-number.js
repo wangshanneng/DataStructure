ef041294fe9e3b55a025772786b22fabd76761da
小王匠 <wsncoder@163.com>
2023-12-29 12:53:08 +0800

    add：9.palindrome-number

---
+++ 9.palindrome-number.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  return x.toString() === x.toString().split("").reverse().join("");
+};
+// @lc code=end
